7659a5a691c49d097313c8d268d44e201f34bc94
Jacob Larfors <jacob@larfors.com>
2016-11-07 11:48:56 +0200

    updated licence

--- kwserverfilters.py
+++ kwserverfilters.py
@@ -1,12 +1,25 @@
-# **************************************************************************************************
-#  Emenda Nordic AB.
+# MIT License
 #
-# Disclaimer: Please note that this software or software component is released by Emenda Nordic AB
-# on a non-proprietary basis for commercial or non-commercial use with no warranty. Emenda Nordic AB
-# will not be liable for any damage or loss caused by the use of this software. Redistribution is
-# only allowed with prior consent.
+# Copyright (c) 2016 Emenda Nordic
 #
-# **************************************************************************************************
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 from kwplib import kwplib
 
